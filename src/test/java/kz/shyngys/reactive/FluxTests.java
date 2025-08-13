142ae1177c291b1c4a4486e42a01159621915a23
rilmayer <yessenbayev.chingis@gmail.com>
2025-08-13 20:22:26 +0500

    - merge, mergeWith, zip, zipToObject fluxes

--- src/test/java/kz/shyngys/reactive/FluxTests.java
+++ src/test/java/kz/shyngys/reactive/FluxTests.java
@@ -1,8 +1,10 @@
 package kz.shyngys.reactive;
 
+import org.assertj.core.groups.Tuple;
 import org.junit.jupiter.api.Test;
 import reactor.core.publisher.Flux;
 import reactor.test.StepVerifier;
+import reactor.util.function.Tuple2;
 
 import java.time.Duration;
 import java.util.ArrayList;
@@ -96,6 +98,68 @@ public class FluxTests {
                 .expectNext(2L)
                 .expectNext(3L)
                 .expectNext(4L)
+                .verifyComplete();
+    }
+
+    @Test
+    public void mergeFluxes() {
+        Flux<String> characterFlux = Flux
+                .just("Garfield", "Kojak", "Barbossa")
+                .delayElements(Duration.ofMillis(500));
+
+        Flux<String> foodFlux = Flux
+                .just("Lasagna", "Lollipops", "Apples")
+                .delaySubscription(Duration.ofMillis(250))
+                .delayElements(Duration.ofMillis(500));
+
+        Flux<String> mergeFlux = characterFlux.mergeWith(foodFlux);
+
+        StepVerifier.create(mergeFlux)
+                .expectNext("Garfield")
+                .expectNext("Lasagna")
+                .expectNext("Kojak")
+                .expectNext("Lollipops")
+                .expectNext("Barbossa")
+                .expectNext("Apples")
+                .verifyComplete();
+    }
+
+    @Test
+    public void zipFluxes() {
+        Flux<String> characterFlux = Flux
+                .just("Garfield", "Kojak", "Barbossa");
+
+        Flux<String> foodFlux = Flux
+                .just("Lasagna", "Lollipops", "Apples");
+
+        Flux<Tuple2<String, String>> zippedFlux = Flux.zip(characterFlux, foodFlux);
+
+        StepVerifier.create(zippedFlux)
+                .expectNextMatches(p
+                        -> p.getT1().equals("Garfield") &&
+                        p.getT2().equals("Lasagna"))
+                .expectNextMatches(p
+                        -> p.getT1().equals("Kojak") &&
+                        p.getT2().equals("Lollipops"))
+                .expectNextMatches(p
+                        -> p.getT1().equals("Barbossa") &&
+                        p.getT2().equals("Apples")).verifyComplete();
+    }
+
+    @Test
+    public void zipFluxesToObject() {
+        Flux<String> characterFlux = Flux
+                .just("Garfield", "Kojak", "Barbossa");
+
+        Flux<String> foodFlux = Flux
+                .just("Lasagna", "Lollipops", "Apples");
+
+        Flux<String> zippedFlux = Flux.zip(characterFlux, foodFlux, (c, f) -> c + " eats " + f);
+
+        StepVerifier.create(zippedFlux)
+                .expectNext("Garfield eats Lasagna")
+                .expectNext("Kojak eats Lollipops")
+                .expectNext("Barbossa eats Apples")
                 .verifyComplete();
     }
 }
